2d1f4920c2126d562899273d90ab8e5971a87b05
XBAROX97 <XBAROX97@GMAIL.COM>
2023-05-17 17:08:04 +0200

    hghj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
-import ourPic from 'public/ourPic.svg'
-import LoadingPage from './loadig';
-import Image from 'next/image';
-
-
+import ourPic from "../public/ourPic.svg";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-
-    <>
-      <h1>welcome to barwa</h1>
-      <Image height={80} width={80} src={ourPic } alt="Image" />  
-      <LoadingPage />
-    </>
-  )
-  }
-
-
+    <div className="bg-cover bg-center h-screen overflow-hidden" style={{ backgroundImage: "url(https://cdn.pixabay.com/photo/2017/08/06/23/47/people-2597664_960_720.jpg)" }}>
+    <div className="container mx-auto px-4 py-8">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-4xl font-bold text-white mb-4">
+          Welcome to Our Company
+        </h1>
+        <p className="text-lg text-white mb-8">
+          We are a trusted provider of high-quality products to meet your needs.
+        </p>
+      </div>
+    </div>
+  </div>
+  
+  );
+};
 
 export default HomePage;
